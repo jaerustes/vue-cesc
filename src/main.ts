6a8bfb5ce4489df456d71caa145f87f37191127d
Jose Antonio <jaerustes@iesjosepsuredaiblanes.com>
2024-11-20 13:30:39 +0100

    segundo

--- src/main.ts
+++ src/main.ts
@@ -6,9 +6,11 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
-import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap';
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css';
+
+import BootstrapVue3 from 'bootstrap-vue-3';
 
 const app = createApp(App)
 
